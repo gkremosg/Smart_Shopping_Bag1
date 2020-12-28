e6be05ddec4b2c69b31d4d766a38492de9979554
gkremosg <gkremosg@gmail.com>
2020-12-28 19:17:42 +0100

    Test Code synchronisation

---
+++ app/src/main/java/com/example/smartshoppingbag/Connection/ConnectionClass.java
@@ -0,0 +1,8 @@
+package com.example.smartshoppingbag.Connection;
+
+public class ConnectionClass {
+    //public static String ip = “”;
+    public static String un = "admin";
+    public static String pass = "1234";
+    public static String db = "";
+}
